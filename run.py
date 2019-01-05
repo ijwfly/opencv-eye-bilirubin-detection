47f73220ade50e7977a858c1bb27060218c0d028
Maxim Nechaev <maxnecha@gmail.com>
2019-01-05 17:50:04 +0300

    some basic scripts

---
+++ run.py
@@ -0,0 +1,29 @@
+import cv2.cv2 as cv2
+import numpy as np
+
+
+def show_image(image):
+    cv2.imshow('image', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    image = cv2.imread('test_images/front.jpg', cv2.IMREAD_COLOR)
+    output = image.copy()
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 150)
+    if circles is not None:
+        # convert the (x, y) coordinates and radius of the circles to integers
+        circles = np.round(circles[0, :]).astype("int")
+
+        # loop over the (x, y) coordinates and radius of the circles
+        for (x, y, r) in circles:
+            # draw the circle in the output image, then draw a rectangle
+            # corresponding to the center of the circle
+            cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+            cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+
+        # show the output image
+        cv2.imshow("output", np.hstack([image, output]))
+        cv2.waitKey(0)
